a721f0d9039be734d894de8d6a111354baa9af5e
sriram1998 <xmenram@gmail.com>
2017-12-17 00:23:48 +0530

    login card created

--- dRegister.php
+++ dRegister.php
@@ -6,9 +6,9 @@ $pass1=$_POST["password"];
 $pass=password_hash($pass1,PASSWORD_BCRYPT);
 $email=$_POST['email'];
 $dob=$_POST['dob'];
-$servername = "localhost";
+$servername = "localhost:3307";
 $username = "root";// Enter mysql username
-$password = "AthlonY2";// Enter mysql password
+$password = "";// Enter mysql password
 $dbname = "hospital";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -16,7 +16,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT name,mail FROM PATIENTS";
+$sql = "SELECT name,mail FROM USERS";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
@@ -38,13 +38,14 @@ if ($result->num_rows > 0) {
 // prepare and bind
 if($error==0)
 {
-$stmt = $conn->prepare("INSERT INTO PATIENTS (name, pass, mail, dob) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("ssss", $new_name, $new_pass, $new_email, $new_dob);
+$stmt = $conn->prepare("INSERT INTO USERS (name, pass, mail, dob ,type) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("sssss", $new_name, $new_pass, $new_email, $new_dob, $new_type);
 // set parameters and execute
 $new_name = $name;
 $new_pass = $pass;
 $new_email = $email;
 $new_dob = $dob;
+$new_type="doctor";
 $stmt->execute();
 //$_SESSION['username']=$name;
 $stmt->close();
--- users.php
+++ users.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
+$servername = "localhost:3307";
 $username = "root";//enter mysql username
-$password = "AthlonY2";//enter mysql password
+$password = "";//enter mysql password
 $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -14,26 +14,27 @@ if ($conn->query($sql) === TRUE) {
 $conn->close();
 ?>
 <?php
-$servername = "localhost";
+$servername = "localhost:3307";
 $username = "root";//enter mysql username
-$password = "AthlonY2";//enter mysql password
+$password = "";//enter mysql password
 $dbname = "hospital";
 // Create connection
 
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-$sql="DROP TABLE PATIENTS";
+$sql="DROP TABLE USERS";
 mysqli_query($conn, $sql);
-$sql = "CREATE TABLE PATIENTS(
+$sql = "CREATE TABLE USERS(
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30) NOT NULL,
 pass VARCHAR(100) NOT NULL,
 mail VARCHAR(70) NOT NULL,
 dob VARCHAR(20) NOT NULL,
+type VARCHAR(10) NOT NULL,
 reg_date TIMESTAMP
 )";
 if (mysqli_query($conn, $sql)) {
-    echo  "Table PATIENTS created successfully  ";
+    echo  "Table USERS created successfully  ";
 } else {
     echo "Error creating table: " . mysqli_error($conn);
 }
